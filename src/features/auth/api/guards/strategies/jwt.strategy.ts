ea02b13e173252269f047f451770cb5a3b65464b
Nickolay <arbuzov.n@f-case.ru>
2023-04-03 12:01:44 +0300

    fix: env for jwt

--- src/features/auth/api/guards/strategies/jwt.strategy.ts
+++ src/features/auth/api/guards/strategies/jwt.strategy.ts
@@ -1,14 +1,17 @@
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { PassportStrategy } from '@nestjs/passport';
 import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
-  constructor() {
+  constructor(
+    public configService: ConfigService
+  ) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: process.env.JWT_SECRET,
+      secretOrKey: configService.get('JWT_SECRET'),
     });
   }
 
