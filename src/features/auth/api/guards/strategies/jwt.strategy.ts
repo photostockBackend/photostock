7b631ac532286fc5439946a6923c04c26431a23c
nickolayArbuzov <56513866+nickolayArbuzov@users.noreply.github.com>
2023-04-30 17:17:38 +0300

    Update jwt.strategy.ts

--- src/features/auth/api/guards/strategies/jwt.strategy.ts
+++ src/features/auth/api/guards/strategies/jwt.strategy.ts
@@ -6,7 +6,7 @@ import { ConfigService } from '@nestjs/config';
 export class BearerAuthGuard extends AuthGuard('jwt') {}
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
-  console.log('configService.get('JWT_SECRET')', configService.get('JWT_SECRET'))
+  console.log("configService.get('JWT_SECRET')", configService.get('JWT_SECRET'))
   constructor(public configService: ConfigService) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
