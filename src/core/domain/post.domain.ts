525294effcc2f5e78f197965d9bc7ebbaf44b3f8
illuhs <illuhs@yandex.ru>
2023-05-16 22:59:19 +0300

    refactor: post domain

--- src/core/domain/post.domain.ts
+++ src/core/domain/post.domain.ts
@@ -3,18 +3,23 @@ import {
   PostUserCreateType,
   PostUserFoundType,
 } from '../../features/user/types/posts/post-user.type';
+import { PostFileDomain } from './post-file.domain';
 
 @Injectable()
 export class PostDomain {
   id: number;
   description: string;
-  postFiles: string[];
+  postFiles: PostFileDomain[];
   userId: number;
 
   static async makeInstanceWithoutId(postDto: PostUserCreateType) {
     const post = new PostDomain();
     post.description = postDto.description;
-    post.postPhotoLinks = postDto.postPhotoLinks;
+    post.postFiles = await Promise.all(
+      postDto.postFiles.map(async (f) =>
+        PostFileDomain.makeInstanceWithoutId(f),
+      ),
+    );
     post.userId = postDto.userId;
     return post;
   }
@@ -22,7 +27,9 @@ export class PostDomain {
     const post = new PostDomain();
     post.id = postDto.id;
     post.description = postDto.description;
-    post.postPhotoLinks = postDto.postPhotoLinks;
+    post.postFiles = await Promise.all(
+      postDto.postFiles.map(async (f) => PostFileDomain.makeInstanceWithId(f)),
+    );
     post.userId = postDto.userId;
     return post;
   }
--- src/features/user/types/posts/post-user.type.ts
+++ src/features/user/types/posts/post-user.type.ts
@@ -1,12 +1,12 @@
+import { PostFileCreateType, PostFileFoundType } from './post-file.types';
+
 export type PostUserCreateType = {
   description: string;
-  postPhotoLinks: string[];
+  postFiles: PostFileCreateType[];
   userId: number;
 };
 
 export type PostUserFoundType = {
   id: number;
-  description: string;
-  postPhotoLinks: string[];
-  userId: number;
-};
+  postFiles: PostFileFoundType[];
+} & PostUserCreateType;
