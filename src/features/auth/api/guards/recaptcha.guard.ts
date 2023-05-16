7e7f28909468e00cc0501d8234b701f316767467
Nickolay <56513866+nickolayArbuzov@users.noreply.github.com>
2023-05-16 17:18:50 +0300

    Update recaptcha.guard.ts

--- src/features/auth/api/guards/recaptcha.guard.ts
+++ src/features/auth/api/guards/recaptcha.guard.ts
@@ -10,10 +10,12 @@ import {
   export class RecaptchaGuard implements CanActivate {
     async canActivate(context: ExecutionContext): Promise<boolean> {
       const { body } = context.switchToHttp().getRequest();
+        console.log('body.recaptchaValue', body.recaptchaValue)
       if (!body.recaptchaValue) throw new ForbiddenException();
       const { data } = await axios.post(
         `https://www.google.com/recaptcha/api/siteverify?response=${body.recaptchaValue}&secret=${process.env.GOOGLE_RECAPTCHA_SECRET_KEY}`,
       );
+        console.log('data', data)
       if (!data.success) throw new ForbiddenException();
       return true;
     }
